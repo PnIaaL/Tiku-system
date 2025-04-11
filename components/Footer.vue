b47d740c37a8fc15e6869e26dee54b6dfe52a202
PnIaaL <166519376+PnIaaL@users.noreply.github.com>
2025-04-11 14:38:39 +0800

    Add files via upload

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-footer>
+    <span>&copy; </span>
+    <br>
+  </el-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ views/index/Dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-scrollbar :native="false" style="height: 100%">
+    <div>
+      <h1 class="title">
+        <a href="#" target="_blank">题库管理系统</a>
+      </h1>
+    </div>
+  </el-scrollbar>
+</template>
+
+<script>
+  export default {
+    name: 'Dashboard',
+  }
+</script>
+
+<style scoped lang="scss">
+  div {
+    animation: leftMoveIn .7s ease-in;
+  }
+
+  @keyframes leftMoveIn {
+    0% {
+      transform: translateX(-100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0%);
+      opacity: 1;
+    }
+  }
+
+  .title {
+    text-align: center;
+    font-size: 25px;
+  }
+
+
+  p {
+    text-indent: 2em;
+  }
+
+  a {
+    text-decoration: none
+  }
+</style>
